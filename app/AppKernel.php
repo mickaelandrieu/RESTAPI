bff4e1207ac64601f1dba2def277d7d0d416326b
Mickaël Andrieu <andrieu.travail@gmail.com>
2014-07-01 01:00:16 +0200

    REST API Standalone v2.0

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,13 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Sw\Bundle\RestSwimmingBundle\SwRestSwimmingBundle(),
-            new Sw\Bundle\SoapCommentBundle\SwSoapCommentBundle(),
-            new Sw\Bundle\ApplicationBundle\SwApplicationBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
-            new BeSimple\SoapBundle\BeSimpleSoapBundle(),
-            new Misd\GuzzleBundle\MisdGuzzleBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
